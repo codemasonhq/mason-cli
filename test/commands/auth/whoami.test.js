70cdeb0185d95893fc2dc3f07d266004739089ef
Ben M <benmag@users.noreply.github.com>
2018-07-03 22:57:50 +1000

    auth:whoami test

---
+++ test/commands/auth/whoami.test.js
@@ -0,0 +1,21 @@
+const {expect, test} = require('@oclif/test')
+const fs = require('fs-extra')
+
+describe('auth:whoami', () => {
+  test
+  .stdout()
+  .stderr()
+  .stub(fs, 'readJSON', () => {
+    return {
+      user: {name: 'Foo Bar', email: 'foo@bar.com'},
+    }
+  })
+  .command(['auth:whoami'])
+  .it('get the current logged in user', ctx => {
+    expect(ctx.stdout).to.equal(
+      'You are currently logged in.\n' +
+      'Name: Foo Bar\n' +
+      'Email: foo@bar.com\n'
+    )
+  })
+})
