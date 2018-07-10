a8ac4305d7a5961ab06c65994ddd8ba9e88ba72c
Ben M <benmag@users.noreply.github.com>
2018-07-10 22:06:30 +1000

    Implement app:destroy test

---
+++ test/commands/apps/destroy.test.js
@@ -0,0 +1,23 @@
+const {expect, test} = require('@oclif/test')
+const fs = require('fs-extra')
+
+describe('apps:destroy', () => {
+  test
+  .stub(fs, 'readJSON', () => {
+    return {
+      endpoint: 'http://localhost',
+      team: {slug: 'test'},
+      user: {token: '123'},
+    }
+  })
+  .nock('http://localhost/v1/test', api => api
+  .delete('/applications/pebble?api_token=123')
+  .reply(200)
+  )
+  .stdout()
+  .stderr()
+  .command(['apps:destroy', 'pebble'])
+  .it('destroy an app', ctx => {
+    expect(ctx.stdout).to.equal('')
+  })
+})
