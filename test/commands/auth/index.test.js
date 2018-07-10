966356af5aaf61892d8bfdca19b593c6f45b7447
Ben M <benmag@users.noreply.github.com>
2018-07-10 22:06:55 +1000

    Implement login and auth:logout tests

---
+++ test/commands/auth/index.test.js
@@ -0,0 +1,49 @@
+const {expect, test} = require('@oclif/test')
+const helpers = require('../../../src/util/helpers')
+const fs = require('fs-extra')
+const os = require('os')
+
+describe('auth', () => {
+  test
+  .stdout()
+  .stderr()
+  .stub(helpers, 'getSSHKey', () => '1234567890')
+  .stub(os, 'hostname', () => 'Test.local')
+  .stub(fs, 'readJSON', () => {
+    return {
+      endpoint: 'http://localhost',
+      user: {email: 'email@example.com'},
+    }
+  })
+  .stub(fs, 'outputJSON', (path, json) => {
+    expect(json).to.have.property('user')
+    expect(json.user.email).to.equal('email@example.com')
+  })
+  .nock('http://localhost/v1', api => api
+  .post('/token', {
+    email: 'email@example.com',
+    password: 'secret123',
+    token_name: 'Mason CLI - Test',
+  })
+  .reply(200, {
+    token: 'abc123',
+    user: {
+      email: 'email@example.com',
+    },
+  })
+  .post('/git/keys', {
+    api_token: 'abc123',
+    title: 'Test.local',
+    key: '1234567890',
+  })
+  .reply(200, [{
+    data: {
+      token: 'abc123',
+    },
+  }])
+  )
+  .command(['auth', '--email', 'email@example.com', '--password', 'secret123'])
+  .it('successful login', ctx => {
+    expect(ctx.stdout).to.equal('Login to your Codemason account\nLogged in as email@example.com\n')
+  })
+})
---
+++ test/commands/auth/logout.test.js
@@ -0,0 +1,30 @@
+const {expect, test} = require('@oclif/test')
+const helpers = require('../../../src/util/helpers')
+const fs = require('fs-extra')
+const os = require('os')
+
+describe('auth:logout', () => {
+  test
+  .stdout()
+  .stderr()
+  .stub(helpers, 'getSSHKey', () => '1234567890')
+  .stub(os, 'hostname', () => 'Test.local')
+  .stub(fs, 'readJSON', () => {
+    return {
+      endpoint: 'http://localhost',
+      user: {token: 'abc123', email: 'email@example.com'},
+    }
+  })
+  .stub(fs, 'outputJSON', (path, json) => {
+    expect(json).to.have.property('endpoint')
+    expect(json.endpoint).to.equal('http://localhost')
+  })
+  .nock('http://localhost/v1', api => api
+  .delete('/git/keys?api_token=abc123&title=Test.local&key=1234567890')
+  .reply(200)
+  )
+  .command(['auth:logout'])
+  .it('successful logout', ctx => {
+    expect(ctx.stdout).to.equal('')
+  })
+})
