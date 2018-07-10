09bce78081735051805fac862c5e4021d0ac7573
Ben M <benmag@users.noreply.github.com>
2018-07-10 22:05:56 +1000

    Implement app:create test

---
+++ test/commands/apps/create.test.js
@@ -0,0 +1,34 @@
+const {expect, test} = require('@oclif/test')
+const helpers = require('../../../src/util/helpers')
+const fs = require('fs-extra')
+
+describe('apps:create', () => {
+  test
+  .stub(fs, 'readJSON', () => {
+    return {
+      endpoint: 'http://localhost',
+      team: {slug: 'test'},
+      user: {token: '123'},
+    }
+  })
+  .stub(helpers, 'createGitRemote', () => {})
+  .nock('http://localhost/v1/test', api => api
+  .post('/applications?api_token=123')
+  .reply(200, {
+    masonVersion: 'v1',
+    type: 'application',
+    name: 'pebble',
+  })
+  )
+  .stdout()
+  .stderr()
+  .command(['apps:create', 'pebble'])
+  .it('create an app', ctx => {
+    expect(ctx.stdout).to.equal(
+      'Creating app on Codemason...\n' +
+      ' ... Created application\n' +
+      ' ... Created remote repository\n' +
+      ' ... Added git remote codemason\n'
+    )
+  })
+})
