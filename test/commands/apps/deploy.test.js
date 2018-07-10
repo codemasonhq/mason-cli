643d0a937e3f5eefbf268745b1222b6cdc65bb24
Ben M <benmag@users.noreply.github.com>
2018-07-10 22:06:13 +1000

    Implement app:deploy test

---
+++ test/commands/apps/deploy.test.js
@@ -0,0 +1,55 @@
+const {expect, test} = require('@oclif/test')
+const fs = require('fs-extra')
+const YAML = require('yamljs')
+const helpers = require('../../../src/util/helpers')
+
+describe('apps:deploy', () => {
+  test
+  .stub(YAML, 'load', () => {
+    return {
+      services: [
+        {
+          name: 'pebble',
+          image: 'registry.mason.ci/pebble/web',
+          environment: {
+            FOO: 'bar',
+          },
+        },
+      ],
+    }
+  })
+  .stub(fs, 'readJSON', () => {
+    return {
+      git: 'git.mason.ci',
+      registry: 'registry.mason.ci',
+      endpoint: 'http://localhost',
+      team: {slug: 'test'},
+      user: {token: '123'},
+    }
+  })
+  .stub(helpers, 'getProjectFromGitRemote', 'test/pebble')
+  .nock('http://localhost/v1/test', api => api
+  .post('/services?application=pebble&api_token=123', {
+    masonVersion: 'v1',
+    type: 'service',
+    name: 0,
+    image: 'registry.mason.ci/pebble/web',
+    environment: {
+      FOO: 'bar',
+    },
+    volumes: [],
+  })
+  .reply(200, {
+    masonVersion: 'v1',
+    type: 'application',
+    name: 'pebble',
+  })
+  )
+  .stdout()
+  .stderr()
+  .command(['apps:deploy', 'pebble'])
+  .it('deploy an app', ctx => {
+    expect(ctx.stdout).to.contain('http://localhost/applications/pebble')
+    expect(ctx.stdout).to.contain('registry.mason.ci/pebble/web')
+  })
+})
