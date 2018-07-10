3c1b95c13cf017948cb3db69615b996c319b1cf9
Ben M <benmag@users.noreply.github.com>
2018-07-10 22:01:51 +1000

    Pass default config to git remote test

--- test/commands/git/remote.test.js
+++ test/commands/git/remote.test.js
@@ -1,10 +1,18 @@
 const {expect, test} = require('@oclif/test')
 const helpers = require('../../../src/util/helpers')
+const fs = require('fs-extra')
 
 describe('git:remote', () => {
   test
   .stdout()
   .stderr()
+  .stub(fs, 'readJSON', () => {
+    return {
+      git: 'git.mason.ci',
+      remote: 'codemason',
+      team: {slug: 'test'},
+    }
+  })
   .stub(helpers, 'createGitRemote', () => {})
   .command(['git:remote', 'pebble'])
   .it('create a git remote', ctx => {
