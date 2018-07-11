74dd6e5f9c340fa85a23870e97f518b7a970724a
Ben M <benmag@users.noreply.github.com>
2018-07-11 20:04:39 +1000

    Implement helpers test

---
+++ test/util/helpers.test.js
@@ -0,0 +1,32 @@
+const {expect, test} = require('@oclif/test')
+const fs = require('fs-extra')
+var child = require('child_process')
+const helpers = require('../../src/util/helpers')
+
+describe('helpers', () => {
+  test
+  .stub(child, 'execSync', () => Buffer.from('codemason\tgit@git.mason.ci:pebble/web (fetch)\ncodemason\tgit@git.mason.ci:pebble/web (push)'))
+  .do(() => {
+    const project = helpers.getProjectFromGitRemote('git.mason.ci')
+    expect(project).to.equal('pebble/web')
+  })
+  .it('getProjectFromGitRemote returns project')
+
+  test
+  .stub(fs, 'existsSync', () => false)
+  .do(() => {
+    expect(helpers.getSSHKey('/fake/path')).to.equal(false)
+  })
+  .it('getSSHKey returns false when not available')
+
+  test
+  .do(() => {
+    var formattedError = helpers.parseApiError({
+      name: ['The name field is required.'],
+      masonVersion: ['The mason version field is required.'],
+    })
+    expect(formattedError).to.contains('The name field is required.')
+    expect(formattedError).to.contains('The mason version field is required.')
+  })
+  .it('parseApiError formats error correctly')
+})
